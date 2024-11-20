9bbae2f632d0b2ad98a7885794c9699f66a725b8
SergioGonzlez <sergoyoyt@gmail.com>
2024-11-20 12:10:20 +0100

    Modificación 1

--- src/sgm/SG.java
+++ src/sgm/SG.java
@@ -5,6 +5,7 @@ public class SG {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Alumno: Sergio González");
+		System.out.println("Primera Modificación desde Master");
 	}
 
 }
